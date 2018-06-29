4f8ff0076c1cb0b232c98c8173d8c854f2a17948
ssemazzijustus1998 <ssemazzijustus@gmail.com>
2018-06-29 17:55:46 +0300

    open the js and start on the very first part of my BDD to display pong for 3

--- js/third.js
+++ js/third.js
@@ -1,2 +1,22 @@
 // bussiness logic
-for(var num =1,num==userInput,)
+var mystuff = function(num){
+  var x ="ping";
+  var y="pong"
+  var xy="pingpong"
+
+  if(num%3===0){
+    return x;
+  }
+
+}
+// UI logic
+$(document).ready(function() {
+$("form#text-form").submit(function(event){
+  event.preventDefault()
+  var userNum = parseInt($("input#text-Input").val());
+  var display = mystuff(userNum);
+  $("#result").text(display);
+  //var result=
+//  $("#result").append("<li>"+userInput+"</li>");
+})
+})
