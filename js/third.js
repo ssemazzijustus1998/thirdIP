3790b7ea5c5f9ab2a02e4ae0a0640defb6b2ddec
ssemazzijustus1998 <ssemazzijustus@gmail.com>
2018-07-02 10:40:39 +0300

    add yord code but separeted into front end and back end add the neccesary ids in html and use them in the js

--- js/third.js
+++ js/third.js
@@ -1,22 +1,37 @@
-// bussiness logic
-var mystuff = function(num){
+// business logic
+
+
+
+function stuff(num){
   var x ="ping";
   var y="pong"
-  var xy="pingpong"
+  var z="ping pong"
 
-  if(num%3===0){
-    return x;
+   if (num%15==0){
+  return z;
   }
+  else if(num%5==0) {
+   return y;
+   }
+   else if(num%3==0){
+    return x;
+   }
+  else{
+    return num;
+   }
+ }
 
-}
-// UI logic
-$(document).ready(function() {
-$("form#text-form").submit(function(event){
-  event.preventDefault()
-  var userNum = parseInt($("input#text-Input").val());
-  var display = mystuff(userNum);
-  $("#result").text(display);
-  //var result=
-//  $("#result").append("<li>"+userInput+"</li>");
-})
-})
+// backend
+
+ $(document).ready(function() {
+ $("#text-form ").submit(function(){
+   event.preventDefault();
+ var blast = $("#text-Input").val();
+ for(var user=1;user<=blast;user++){
+   var display=stuff(user);
+    $("#result").append("<li>"+display+"</li>");
+    // console.log(display);
+ }
+
+ });
+});
